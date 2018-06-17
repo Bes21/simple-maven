7467abfc162d19b5be85bc79024fe5958c41b141
Beslan <bes_job@inbox.ru>
2018-06-18 01:10:00 +0300

    1

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,31 @@
+package main;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.AllRequestsServlet;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+//        Frontend frontend = new Frontend();
+
+//        Server server = new Server(8080);
+//        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+//        server.setHandler(context);
+//        context.addServlet(new ServletHolder(frontend), "/authform");
+//        server.start();
+//        server.join();
+
+        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+
+    }
+}
